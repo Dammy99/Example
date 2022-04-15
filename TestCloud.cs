6c20a69d7833e51b24f375053d21294744bda9b6
Dammy99 <deslyly.prod@gmail.com>
2022-04-15 17:43:57 +0300

    Usint Sonar

--- TestCloud.cs
+++ TestCloud.cs
@@ -1,10 +1,9 @@
-namespace Program
+namespace HelloWorld
 {
     class Hello {         
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello World!");
-            
         }
     }
 }
